5c8ca03faf5d6c279f007a7eb53009c71746a4fa
Cath J <cathj2002@gmail.com>
2020-05-26 22:57:39 -0600

    Added in hand calc eqs, fixed up some test calc errors in perstest

--- perstest.jl
+++ perstest.jl
@@ -3,7 +3,6 @@ using DifferentialEquations
 using Flux
 using LinearAlgebra
 using Plots
-using Interact
 
 ##########
 # Draft code for non-linear implicit euler solver
@@ -46,6 +45,9 @@ function energy(X, theta, u0, p, t)
             (1 + dot(tangent[:, i], tangent[:, i+1]))
         phi[i] = 2 * atan(norm(kb[:, i]) / 2)
     end
+
+    println("this is kb:")
+    println(kb)
 
     # Bishop frame
     u = zeros(Float64, (3, n - 1))
@@ -94,77 +96,77 @@ function energy(X, theta, u0, p, t)
     return Ebend + Etwist
 end
 
-function energydict(
-    X::AbstractArray,
-    theta::AbstractArray,
-    u0::AbstractArray,
-    p,
-)
-    n = p[1]
-    l0 = p[2]
-    B = [1 0; 0 1]
-
-    # edges, tangent, kb, phi
-    edges = Dict{Int32,Array{Float64,1}}()
-    tangent = Dict{Int32,Array{Float64,1}}()
-    kb = Dict{Int32,Array{Float64,1}}()
-    phi = Dict{Int32,Float64}()
-
-    # voronoi domain
-    ell = Dict{Int32,Float64}()
-
-    edges[1] = X[:, 2] - X[:, 1]
-    tangent[1] = normd(edges[1])
-    ell[1] = 0.5 * sqrt(edges[1]' * edges[1])
-
-    m = diff(theta, dims = 1) #Dict(i => theta[i+1] - theta[i] for i in 1:n-1)
-
-    # Bishop frame
-    u = Dict{Int32,Array{Float64,1}}()
-    v = Dict{Int32,Array{Float64,1}}()
-    # Material frame
-    m1 = Dict{Int32,Array{Float64,1}}()
-    m2 = Dict{Int32,Array{Float64,1}}()
-
-    u[1] = normd(u0)
-    v[1] = cross(tangent[1], u[1])
-    m1[1] = cos(theta[1]) * u[1] + sin(theta[1]) * v[1]
-    m2[1] = -sin(theta[1]) * u[1] + cos(theta[1]) * v[1]
-    Ebend = 0.0
-    Etwist = 0.0
-    Estretch = (edges[1] .- l0)' * (edges[1] .- l0)
-    for i = 1:(n-2)
-        edges[i+1] = X[:, i+2] - X[:, i+1]
-        tangent[i+1] = normd(edges[i+1])
-        kb[i] =
-            2 .* cross(tangent[i], tangent[i+1]) /
-            (1 + tangent[i]' * tangent[i+1])
-        kbn = sqrt(kb[i]' * kb[i])
-        phi[i] = 2 * atan(kbn / 2)
-
-        ax = kb[i] / kbn
-
-        ell[i+1] =
-            0.5 * (sqrt(edges[i]' * edges[i]) + sqrt(edges[i+1]' * edges[i+1]))
-
-        u[i+1] =
-            dot(ax, u[i]) * ax +
-            cos(phi[i]) * cross(cross(ax, u[i]), ax) +
-            sin(phi[i]) * cross(ax, u[i])
-        v[i+1] = cross(tangent[i+1], u[i+1])
-        m1[i+1] = cos(theta[i+1]) * u[i+1] + sin(theta[i+1]) * v[i+1]
-        m2[i+1] = -sin(theta[i+1]) * u[i+1] + cos(theta[i+1]) * v[i+1]
-        k = 0.5 * [dot(kb[i], m2[i] + m2[i+1]), -dot(kb[i], m1[i] + m1[i+1])]
-
-        Ebend += k' * B * k / ell[i+1]
-        Etwist += m[i] .^ 2 / ell[i+1]
-        s = edges[i+1] .- l0
-        Estretch += s's
-    end
-    Ebend = 0.5 * Ebend
-    Estretch = 0.5 * Estretch
-    return Ebend + Etwist + Estretch
-end # function
+# function energydict(
+#     X::AbstractArray,
+#     theta::AbstractArray,
+#     u0::AbstractArray,
+#     p,
+# )
+#     n = p[1]
+#     l0 = p[2]
+#     B = [1 0; 0 1]
+#
+#     # edges, tangent, kb, phi
+#     edges = Dict{Int32,Array{Float64,1}}()
+#     tangent = Dict{Int32,Array{Float64,1}}()
+#     kb = Dict{Int32,Array{Float64,1}}()
+#     phi = Dict{Int32,Float64}()
+#
+#     # voronoi domain
+#     ell = Dict{Int32,Float64}()
+#
+#     edges[1] = X[:, 2] - X[:, 1]
+#     tangent[1] = normd(edges[1])
+#     ell[1] = 0.5 * sqrt(edges[1]' * edges[1])
+#
+#     m = diff(theta, dims = 1) #Dict(i => theta[i+1] - theta[i] for i in 1:n-1)
+#
+#     # Bishop frame
+#     u = Dict{Int32,Array{Float64,1}}()
+#     v = Dict{Int32,Array{Float64,1}}()
+#     # Material frame
+#     m1 = Dict{Int32,Array{Float64,1}}()
+#     m2 = Dict{Int32,Array{Float64,1}}()
+#
+#     u[1] = normd(u0)
+#     v[1] = cross(tangent[1], u[1])
+#     m1[1] = cos(theta[1]) * u[1] + sin(theta[1]) * v[1]
+#     m2[1] = -sin(theta[1]) * u[1] + cos(theta[1]) * v[1]
+#     Ebend = 0.0
+#     Etwist = 0.0
+#     Estretch = (edges[1] .- l0)' * (edges[1] .- l0)
+#     for i = 1:(n-2)
+#         edges[i+1] = X[:, i+2] - X[:, i+1]
+#         tangent[i+1] = normd(edges[i+1])
+#         kb[i] =
+#             2 .* cross(tangent[i], tangent[i+1]) /
+#             (1 + tangent[i]' * tangent[i+1])
+#         kbn = sqrt(kb[i]' * kb[i])
+#         phi[i] = 2 * atan(kbn / 2)
+#
+#         ax = kb[i] / kbn
+#
+#         ell[i+1] =
+#             0.5 * (sqrt(edges[i]' * edges[i]) + sqrt(edges[i+1]' * edges[i+1]))
+#
+#         u[i+1] =
+#             dot(ax, u[i]) * ax +
+#             cos(phi[i]) * cross(cross(ax, u[i]), ax) +
+#             sin(phi[i]) * cross(ax, u[i])
+#         v[i+1] = cross(tangent[i+1], u[i+1])
+#         m1[i+1] = cos(theta[i+1]) * u[i+1] + sin(theta[i+1]) * v[i+1]
+#         m2[i+1] = -sin(theta[i+1]) * u[i+1] + cos(theta[i+1]) * v[i+1]
+#         k = 0.5 * [dot(kb[i], m2[i] + m2[i+1]), -dot(kb[i], m1[i] + m1[i+1])]
+#
+#         Ebend += k' * B * k / ell[i+1]
+#         Etwist += m[i] .^ 2 / ell[i+1]
+#         s = edges[i+1] .- l0
+#         Estretch += s's
+#     end
+#     Ebend = 0.5 * Ebend
+#     Estretch = 0.5 * Estretch
+#     return Ebend + Etwist + Estretch
+# end # function
 
 
 function energy_clean(
@@ -204,6 +206,10 @@ function energy_clean(
         edges_1 = X[:, i+2] - X[:, i+1]
         tangent_1 = normd(edges_1)
         kb = 2 .* cross(tangent, tangent_1) / (1 + tangent'tangent_1)
+
+        println("this is kb")
+        println(kb)
+
         kbn = sqrt(kb'kb)
         ell = 0.5 * (sqrt(edges'edges) + sqrt(edges_1'edges_1))
         phi = 2 * atan(kbn / 2)
@@ -300,11 +306,17 @@ NO TWIST, 45° BEND
 GRAPH 1: Straight bend
 """
 
+println("%%%%%%%%%%%%%%%%%%%%%% STRAIGHT BEND %%%%%%%%%%%%%%%%%%%%%%%%%")
+
+#note that theta is in angles ==> CHANGE TO RADIANS
+
 energy_bend = plot()
 en(theta) = energy_clean([0. 1. 1+cos(theta*pi/180);
         0. 0. sin(theta*pi/180);0. 0. 0.],[0,0,0],[0.;1.;0.],[3,1])
+en_hand(theta) = 2*(sin(theta*pi/180)/(1 + cos(theta*pi/180)))^2
 
-plot!(en,0,90)
+plot!(en,0,90, label = "code")
+plot!(en_hand,0,90, label = "hand")
 xlabel!("Angle of bend (°)")
 ylabel!("Energy")
 title!("1: Angle of bend vs. energy")
@@ -317,6 +329,8 @@ p0 = [0. 1. 1;0. 0. 1.;0. 0. 0.]
 TESTING FLUX.GRADIENT
 """
 
+println("%%%%%%%%%%%%%%%%%%%%%% FLUX GRADIENT%%%%%%%%%%%%%%%%%%%%%%%%%")
+
 Ex = x -> energy_clean(x, [0,0,0],[0.,1.,0.], [3,1])
 Et = t -> energy_clean(p0, t, [0.;1.;0.], [3,1])
 
@@ -326,10 +340,14 @@ ft = -1.0 * Flux.gradient(Et, [0,0,0])[1]
 println("this is (fx,ft), bent 90°")
 println(fx,ft)
 
+println("this is bend Energy, bent 90°")
+println(Ex(p0))
+
 """
 GRAPH 2: Smaller  ==> bigger loops
 """
 
+println("%%%%%%%%%%%%%%%%%%%%%% SMALLER ==> BIGGER LOOPS%%%%%%%%%%%%%%%%%%%%%%")
 # x_circle = Array{Float64}()
 
 energy_bend = plot()
@@ -340,17 +358,26 @@ X = zeros(Float64,3,10)
 function en(rad)
     X = zeros(Float64,3,10)
     for i = 1:10
-        X[1,i] = rad * cos(2.0*pi*i/10)
-        X[2,i] = rad * sin(2.0*pi*i/10)
+        X[1,i] = rad * cos(2.0*pi*(i-1)/10)
+        X[2,i] = rad * sin(2.0*pi*(i-1)/10)
     end #for loop
 
     # println("this is positions")
     # println(X)
-    energy = energy_clean(X,zeros(10,1),[-1.;0.;0.],[10,2*rad*sin(pi/10)])
+    n1 = -cos(pi/10)
+    n2 = -sin(pi/10)
+    energy = energy_clean(X,zeros(10,1),[n1;n2;0.],[10,sqrt(2*rad^2*(1-cos(2*pi/10)))])
     return energy
 end #function
 
-plot!(en,1,10, xaxis=:log, yaxis=:log)
+sin_i = sin(2.0*pi/10)
+cos_i = cos(2.0*pi/10)
+
+en_hand(rad) = (2.0/ (sqrt(2*rad^2*(1-cos(2*pi/10))))) * (10 - 2) * (sin_i/(1+cos_i))^2
+
+# plot!(en,1,10, xaxis=:log, yaxis=:log)
+plot!(en,1,10, label = "code")
+plot!(en_hand,1,10, label = "hand")
 xlabel!("Radius of loop")
 ylabel!("Bend + twist energy")
 title!("2: Radius of loop vs. energy")
@@ -358,10 +385,11 @@ title!("2: Radius of loop vs. energy")
 display(energy_bend)
 
 
-
 """
 GRAPH 3: Straight rod w/ twist
 """
+
+println("%%%%%%%%%%%%%%%%%%%%%% STRAIGHT ROD W/ TWIST%%%%%%%%%%%%%%%%%%%%%%%%%")
 
 energy_tot = plot()
 # en(theta) = energy_clean([0. 1. 1+cos(theta);
@@ -373,7 +401,8 @@ function en(rad)
     return energy
 end #function
 
-plot!(en,10^-6,pi,xaxis=:log, yaxis=:log)
+# plot!(en,10^-6,pi,xaxis=:log, yaxis=:log)
+plot!(en,10^-6,pi)
 xlabel!("Angle of twist (rad)")
 ylabel!("Energy")
 title!("3: Angle twist in straight rod vs. energy")
@@ -383,6 +412,8 @@ display(energy_tot)
 """
 GRAPH 4: Straight rod w/ twist throughout
 """
+
+println("%%%%%%%%%%%%%%%%%%%%%% STRAIGHT ROD W/ TWIST THROUGHOUT%%%%%%%%%%%%%")
 
 energy_tot = plot()
 # en(theta) = energy_clean([0. 1. 1+cos(theta);
@@ -394,7 +425,8 @@ function en(rad)
     return energy
 end #function
 
-plot!(en,10^-6,pi,xaxis=:log, yaxis=:log)
+# plot!(en,10^-6,pi,xaxis=:log, yaxis=:log)
+plot!(en,10^-6,pi)
 xlabel!("Angle of twist (rad)")
 ylabel!("Energy")
 title!("3: Angle twist in straight rod vs. energy")
