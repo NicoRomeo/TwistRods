879654817adcb30dca58ce111e7dcfc41362e9a1
NicoRomeo <nromeo@mit.edu>
2020-05-28 14:33:56 -0400

    staging for merge

--- test.jl
+++ test.jl
@@ -32,13 +32,24 @@ function that runs a single timestep
     args: F force function
     state
 """
-function timestep(F, state::Array{Float64,1})
-
+function timestep(Fx, Ft, state::Array{Float64,1}, n::Int, dt::Float64)
+    # unpack state
+    x, theta, u0 = newrod.state2vars(state, n)
     # Compute explicit initial guess for X, theta at t+dt
+
+    x_g  = dt* Fx(x, theta, u0) + x
+    t_g = dt* Ft(x, theta, u0) + theta
 
     # Using the initial gues, use a NL solver to get (X, theta) at t+dt
 
+    aux1(x_1) = dt * Fx(x_1, theta, u0) - x
+    aux2(t_1) = dt * Ft(x, t, u0) - theta
+
+    solve ...
     # update u_0
+
+    t0 = pos[2,:] - pos[1,:]
+    R = newrod.rotab(t0, t0 + fu0)
 
     # package things use, and return the new state
 end # function
