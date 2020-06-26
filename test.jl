2eea0cb3426b7c6c770c23b3bb8567d33072f265
NicoRomeo <nromeo@mit.edu>
2020-06-26 13:30:39 -0400

    removed time parallel code from test.jl. We should rename that file

--- test.jl
+++ test.jl
@@ -9,6 +9,7 @@ using Plots
 #using Flux
 using Zygote
 using LinearAlgebra
+using NLsolve
 
 include("newrod.jl")
 
@@ -83,75 +84,7 @@ function energy_q(q, u0::Array{Float64,1}, p)
     return Ebend + Etwist #+ Estretch
 end # function
 
-function energy_timep(q, u, p)
-    n = Int(p[1])
-    l0 = p[2]
-    B = [1 0; 0 1]
 
-    # edges, tangent, kb, phi
-    #X = reshape(q[1:3*n], (3,n))
-    X = q[1:3*n]
-    theta = q[3*n+1:end]
-
-    #edges = X[:, 2] - X[:, 1]
-    edges = q[4:6] - q[1:3]
-    tangent = normd(edges)
-    ell = 0.5 * sqrt(edges'edges)
-
-    m = diff(theta, dims = 1) #Dict(i => theta[i+1] - theta[i] for i in 1:n-1)
-
-    #u = normd(u0)
-    v = cross(tangent, u[1:3])
-    m1 = cos(theta[1]) * u[1:3] + sin(theta[1]) * v
-    m2 = -sin(theta[1]) * u[1:3] + cos(theta[1]) * v
-    Ebend = 0.0
-    Etwist = 0.0
-    s = sqrt(edges'edges) - l0
-    Estretch = s's
-    # inner points
-    #i = 2
-    #while i < n
-    for i = 2:(n-1)
-        #edges_1 = X[:, i+2] - X[:, i+1]
-        edges_1 = q[3*i+1:3*(i+1)] - q[3*(i-1)+1:3*i]
-
-        tangent_1 = normd(edges_1)
-        kb = 2 .* cross(tangent, tangent_1) / (1 + tangent'tangent_1)
-        kbn = sqrt(kb'kb)
-        ell = 0.5 * (sqrt(edges'edges) + sqrt(edges_1'edges_1))
-        u = if !isapprox(kbn, 0.0)
-
-            phi = 2 * atan(dot(kb, kb / kbn) / 2)
-            ax = kb / kbn
-            u =
-                dot(ax, u) * ax +
-                cos(phi) * cross(cross(ax, u), ax) +
-                sin(phi) * cross(ax, u)
-        end
-        v = cross(tangent_1, u[3*i+1:3*(i+1)])
-        m1_1 = cos(theta[i+1]) * u[3*i+1:3*(i+1)] + sin(theta[i+1]) * v
-        m2_1 = -sin(theta[i+1]) * u[3*i+1:3*(i+1)] + cos(theta[i+1]) * v
-        k = 0.5 * [dot(kb, m2 + m2_1), -dot(kb, m1 + m1_1)]
-
-        Ebend += k' * B * k / ell
-        Etwist += m[i] .^ 2 / ell
-        s = sqrt(edges_1'edges_1) .- l0
-        Estretch += s * s
-        # update for next vertex
-        edges = edges_1
-        tangent = tangent_1
-        m1 = m1_1
-        m2 = m2_1
-    end
-    #final point
-    ell = 0.5 * sqrt(edges'edges)
-    Etwist += m[end] .^ 2 / ell
-    Ebend = 0.5 * Ebend
-    Estretch = 0.5 * Estretch
-    Etwist = 0.5 * Etwist
-    return Ebend + Etwist + 3 * Estretch
-
-end
 
 function skewmat(a::Array{Float64})
     return [0 -a[3] a[2]; a[3] 0 -a[1]; -a[2] a[1] 0]
@@ -170,12 +103,6 @@ function rotab(a::Array{Float64,1}, b::Array{Float64,1}, c::Array{Float64,1})
 end # function
 
 
-# function energy_q(q, u0::Array{Float64,1}, p)
-#     n = p[1]
-#     x = reshape(q[1:3*n], (3, n))
-#     theta = q[3*n+1:end]
-#     return energy_clean(x, theta, u0, p)
-# end
 
 function F!(f::Array{Float64,1}, q::Array{Float64,1}, u0, param)
     E = mu -> energy_q(mu, u0, param)
@@ -255,6 +182,7 @@ function timestep(
     return vcat(u_fin, x_fin) #vcat(u4, q4),
 
 end # function
+
 
 
 function main()
@@ -665,6 +593,5 @@ function main()
     # end every 5
 
 end
-
 
 main()
